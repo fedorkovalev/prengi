6ad3dd808d76bf9f29bd06d02bc9d513b789b0ba
Fedor <deadkovalev@yandex.ru>
2020-05-05 00:26:16 +0300

    add _hosts

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
         speed: 1200,
         autoplay: true,
         autoplaySpeed: 2000,
-        dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.svg"></button>',
     });
