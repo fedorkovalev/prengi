b0bd607d30153a29c95e9fa294ffc4eb414d0436
Fedor <deadkovalev@yandex.ru>
2020-06-02 12:28:18 +0300

    so lazy but full adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 769,
                 settings: {
                     dots: true,
                 }
@@ -31,7 +31,7 @@ window.addEventListener('DOMContentLoaded', () => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             rowHeader.classList.toggle('row__header_active');
-        })
-    })
-})
+        });
+    });
+});
   });
